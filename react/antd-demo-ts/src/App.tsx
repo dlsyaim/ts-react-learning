0ce386f015c5c772b38fe54ae5aedf373780ec57
dlsyaim <dlsyaim@gmail.com>
2018-07-23 17:09:41 +0800

    ts-react init

---
+++ react/antd-demo-ts/src/App.tsx
@@ -0,0 +1,17 @@
+// import Button from 'antd/lib/button';
+import {Button} from 'antd';
+import * as React from 'react';
+
+import './App.css';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="App">
+       <Button type="primary">Button</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
